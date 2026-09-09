3104fb08748ea4a02e4196c6a1ef731e98951b57
manuasdf <git@manuasdf.dev>
2026-09-09 12:48:52 +0200

    feat[nextjs]: Expose deliveries as API routes

--- 07_Nextjs/02_client-server/kikis-delivery-service/app/deliveries/new/page.tsx
+++ 07_Nextjs/02_client-server/kikis-delivery-service/app/deliveries/new/page.tsx
@@ -1,17 +1,6 @@
-import { createDelivery } from "@/lib/services/deliveriesService";
-import { revalidatePath } from "next/cache";
+import { addDelivery } from "@/app/actions";
 
 export default async function NewDeliveryPage() {
-  async function addDelivery(formData: FormData) {
-    "use server";
-
-    const pickup = formData.get("pickup") as string;
-    const destination = formData.get("destination") as string;
-
-    await createDelivery({ pickup, destination });
-    revalidatePath("/deliveries");
-  }
-
   return (
     <form action={addDelivery}>
       <input name="pickup" placeholder="Pickup" />
